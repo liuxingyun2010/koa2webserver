1cf727fe0047286a720cb26c983120bd2a63781d
xingyun.liu <xingyun.liu@atzuche.cn>
2017-08-22 11:41:52 +0800

    update

--- app/routers/index.js
+++ app/routers/index.js
@@ -2,16 +2,28 @@ var routesLoader = require('../utils/routesLoader')
 var userRoute = require('./user')
 var groupRoute = require('./group')
 var dailyRoute = require('./daily')
-var testRoute = require('./static')
+var staticRoute = require('./static')
 
 
 module.exports = function(app) {
 	app
 		.use(userRoute.routes())
-		.use(groupRoute.routes())
-		.use(dailyRoute.routes())
-		.use(testRoute.routes())
 		.use(userRoute.allowedMethods({
+			throw: true,
+		}))
+	app
+		.use(groupRoute.routes())
+		.use(groupRoute.allowedMethods({
+			throw: true,
+		}))
+	app
+		.use(dailyRoute.routes())
+		.use(dailyRoute.allowedMethods({
+			throw: true,
+		}))
+	app
+		.use(staticRoute.routes())
+		.use(staticRoute.allowedMethods({
 			throw: true,
 		}))
 }
