6676074f2619910db655ad683b3c4a4d1e131cf2
xingyun.liu <xingyun.liu@atzuche.cn>
2017-08-24 18:07:13 +0800

    update

--- app/config/index.js
+++ app/config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
 	port: process.env.PORT || 80,
-	// connectString: 'mongodb://localhost:27017/dailydb',
+	connectString: 'mongodb://localhost:27017/dailydb',
 	// connectString: 'mongodb://dbdev:db1234567@10.0.3.207:27017/daily', 
-	connectString: 'mongodb://dev:dev1qaz2wsx@106.15.126.244:27017/daily',
+	// connectString: 'mongodb://dev:dev1qaz2wsx@106.15.126.244:27017/daily',
 	baseApi: 'api',
 	jwtKey: '!@#zaq2017'
 }
