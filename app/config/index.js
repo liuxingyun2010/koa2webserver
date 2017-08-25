386557f50dd9c79cf22f7f49c888aa7c317467ab
liuxingyun <liuxingyun2010@gmail.com>
2017-08-26 00:38:09 +0800

    update

--- app/config/index.js
+++ app/config/index.js
@@ -5,7 +5,7 @@ const config = {
 	},
 	test: {
 		port: 3000,
-		connectString: 'mongodb://dbdev:db1234567@127.0.0.1:27017/daily', 
+		connectString: 'mongodb://127.0.0.1:27017/daily', 
 	},
 	develop:{
 		port: 3333,
